25ce8d74d9d344bee616a266b534b1f60c29ce14
LauraKirby <laurakirby26@gmail.com>
2015-07-14 23:25:15 -0700

    add logic to show an author's books to author show page, add private, new and create methods to book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,22 @@
 class BooksController < ApplicationController
+  before_action :find_author, only: [:index, :new, :create]
+  before_action :find_animal, only: [:show, :edit, :update, :destroy]
   def index
   	#no need for an animales index, right?
   end
 
   def new
+    @book = Book.new
   end
 
   def create 
+    @book = @author.books.new book_params
+    if @book.save
+      flash[:success] = "A book was created"
+      redirect_to author_books_path(@animal)
+    else 
+      render :new
+    end
   end 
 
   def show
@@ -20,6 +30,20 @@ class BooksController < ApplicationController
 
   def destroy 
   end 
+
+private
+  
+  def find_author 
+    @author = Author.find params[:author_id]
+  end 
+
+  def find_book
+    @book = Book.find params[:id]
+  end 
+
+  def book_params
+    params.require(:book).permit(:title, :description)
+  end
 
 
 end
