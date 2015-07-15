843f52b085bf8dfbca4f32821b15293b067e51a2
LauraKirby <laurakirby26@gmail.com>
2015-07-14 22:16:25 -0700

    add index page and empty delete, update and create methods to controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,13 +1,23 @@
 class AuthorsController < ApplicationController
   def index
+  	@authors = Author.all
   end
 
   def new
   end
+
+  def create 
+  end 
 
   def show
   end
 
   def edit
   end
+
+  def update
+  end 
+
+  def destroy 
+  end 
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,25 @@
 class BooksController < ApplicationController
   def index
+  	#no need for an animales index, right?
   end
 
   def new
   end
+
+  def create 
+  end 
 
   def show
   end
 
   def edit
   end
+
+  def update
+  end 
+
+  def destroy 
+  end 
+
+
 end
